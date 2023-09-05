581667cbcb3a403592cbd499591ecbbc37bc2078
Denis <denistodias1@gmail.com>
2023-09-04 22:18:24 -0300

    API

---
+++ script.sql
@@ -0,0 +1,10 @@
+use infocdb;
+
+create table tb_senha(
+	id_senha int primary key auto_increment,
+    ds_senha varchar(400) not null,
+    ds_nsenha varchar(400) not null
+
+);
+
+select * from tb_senha;
